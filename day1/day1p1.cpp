2d9cca8c2d4942c3755c12b41c3f458ee5bec362
ceej <cory1993johnson@gmail.com>
2025-11-30 14:52:26 -0800

    day1 prep

--- day1/day1p1.cpp
+++ day1/day1p1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char** argv) {
     std::string path = (argc > 1) ? argv[1] : "input.txt";
@@ -9,6 +13,18 @@ int main(int argc, char** argv) {
         std::cerr << "Failed to open '" << path << "'\n";
         return 2;
     }
+
+    // Using Example input
+    string test_input = R"()";
+
+    // Using Real input
+    //string input{};
+    //string str{};
+    //while (getline(in,str))
+    //{
+        //input.append(str + '\n');
+    //}
+    
 
     return 0;
 }
