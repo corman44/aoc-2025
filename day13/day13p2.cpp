f4761598cbdaa59a542633c557b21b1b50c74733
ceej <cory1993johnson@gmail.com>
2025-11-30 11:17:06 -0800

    working on day13-2022

---
+++ day13/day13p2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv) {
+    std::string path = (argc > 1) ? argv[1] : "input.txt";
+    std::ifstream in(path);
+    if (!in) {
+        std::cerr << "Failed to open '" << path << "'\n";
+        return 2;
+    }
+
+    return 0;
+}
--- day16/day16p1.cpp
+++ day16/day16p1.cpp
@@ -10,26 +10,12 @@
 #include <tuple>
 #include <regex>
 
-struct Valve {
-    std::string name;
-    int flow;
-
-    Valve(std::string name, int flow) : name(name), flow(flow) {}
-
-    bool operator==(Valve const& other) const noexcept {
-        return name == other.name && flow == other.flow;
-    }
+enum TunnelDecision {
+    OPEN,
+    LOWPRIO,
+    HIGHPRIO,
+    COURSE,
 };
-
-namespace std {
-    template<>
-    struct hash<Valve> {
-        size_t operator()(Valve const& v) const noexcept
-        {
-            return std::hash<std::string>{}(v.name) ^ (std::hash<int>{}(v.flow) << 1);
-        }
-    };
-}
 
 void print_tunnels(std::unordered_map<std::string, std::tuple<int, std::vector<std::string>>>& tunnels)
 {
@@ -47,7 +33,7 @@ void print_tunnels(std::unordered_map<std::string, std::tuple<int, std::vector<s
     }
 }
 
-void parse_input(std::string input)
+std::unordered_map<std::string, std::tuple<int, std::vector<std::string>>>  parse_input(std::string input)
 {
     std::regex pattern(R"([A-Z]{2})");
     
@@ -101,7 +87,8 @@ void parse_input(std::string input)
         }
     }
 
-    print_tunnels(tunnels);
+    //print_tunnels(tunnels);
+    return tunnels;
 }
 
 int main(int argc, char** argv)
@@ -124,20 +111,24 @@ Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II)";
 
-    // Test Input
-    // std::istringstream stream(test_input);
-    // std::vector<std::string> lines{};
-    // std::string str{};
-    // while(getline(stream,str))
-    // {
-        // lines.push_back(str);
-    // }
 
     // Create Map Data
     // ('Name', flow) : list of connected tunnels
-    // ('AA', 0) : ['DD', 'II', 'BB']
-    std::unordered_map<std::string, std::tuple<int, std::vector<std::string>>> valves{};
-    parse_input(test_input);
+    // 'AA' : (0, ['DD', 'II', 'BB'])
+    auto tunnels = parse_input(test_input);
+
+    // Create Path
+    /// GAMEPLAN
+    /// BFS of tunnels to determine if flow > 10 and to add to open valve
+    /// if all are zeros, then need to check all flows of next values and choose highest of that group (hoping multiples of all zeros don't happen)
+
+    //std::string real_input{};
+    //std::string str{};
+    //while (getline(in, str))
+    //{
+        //real_input.append(str + '\n');
+    //}
+    //parse_input(real_input);
 
     // Find most efficient route
 
