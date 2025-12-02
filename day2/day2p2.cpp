39b38499c6a20f1a8aa67fc3e723c4fbd6b1c06b
ceej <ceejpohang@gmail.com>
2025-12-02 12:49:21 -0800

    done :)

--- day2/day2p2.cpp
+++ day2/day2p2.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -13,17 +14,29 @@ struct Range {
     uint64_t end;
 };
 
-bool is_valid(uint64_t num)
+int num_digits(uint64_t num)
 {
-    // Attempting as String
+    return (int)log10(num) + 1;
+}
+
+bool is_valid(uint64_t num, int x)
+{
     auto str = to_string(num);
     auto len = str.length();
 
-    if (len % 2 == 0)
+    // Check that len / x has no remainder
+    if (len % x == 0)
     {
-        string first = str.substr(0,len/2);
-        string sec = str.substr(len/2);
-        return first != sec;
+        for (int i = 0; i < (len / x) - 1; ++i)
+        {
+            auto first = str.substr(i*x, x);
+            auto second = str.substr((i+1)*x, x);
+            if (first != second)
+            {
+                return true;
+            }
+        }
+        return false;
     } else {
         return true;
     }
@@ -70,9 +83,16 @@ vector<uint64_t> gather_invalid_ids(vector<Range> ranges)
     {
         for (auto num = range.start; num <= range.end; num++)
         {
-            if (!is_valid(num) && !(to_string(num).length() % 2))
+            if(num_digits(range.start) > 3) {
+                cout << "";
+            }
+            for(auto len = 1; len <= num_digits(num)/2; ++len)
             {
-                invalids.push_back(num);
+                if (!is_valid(num, len))
+                {
+                    invalids.push_back(num);
+                    break;
+                }
             }
         }
     }
@@ -89,15 +109,15 @@ int main(int argc, char** argv) {
     }
 
     // Using Example input
-    string input = R"(11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124)";
+    //string input = R"(11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124)";
 
     // Using Real input
-    //string input{};
-    //string str{};
-    //while (getline(in,str))
-    //{
-        //input.append(str);
-    //}
+    string input{};
+    string str{};
+    while (getline(in,str))
+    {
+        input.append(str);
+    }
 
     auto ranges = parse_input(input);
     auto invalids = gather_invalid_ids(ranges);
@@ -108,7 +128,18 @@ int main(int argc, char** argv) {
         sum += num;
     }
 
-    cout << "Sum Invalids: " << sum << endl; 
+    // Testing Test
+    //uint64_t ans1 = 1227775554;
+    uint64_t ans = 4174379265;
+    if (ans == sum)
+    {
+        cout << "Answer Found: " << sum << endl; 
+    } else {
+        cout <<"Wrong Answer: " << sum << " != " << ans << endl;
+    }
+    cout << "Sum Invalids: " << sum << endl;
+    
+
 
     return 0;
 }
