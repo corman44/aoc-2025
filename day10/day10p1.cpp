960ca80608751059fab0027144d8282062fd0f3b
ceej <cory1993johnson@gmail.com>
2025-12-21 14:54:01 -0800

    day10p1 wip

--- day10/day10p1.cpp
+++ day10/day10p1.cpp
@@ -10,17 +10,17 @@ using namespace std;
 
 /// @brief simple struct containing lights_status and num lights
 struct Inidicator {
-    uint16_t light_status{}, num_lights{}, light_goal{};
+    uint16_t  light_status{}, num_lights{}, light_goal{};
 };
 
 
 struct LightDiagram {
     Inidicator indicator{};
-    vector<vector<size_t>> buttons{};
-    //vector<size_t> jolts;
+    vector<vector<uint16_t>> buttons{};
+    //vector<uint16_t> jolts;
 };
 
-size_t str_to_int(string str)
+uint16_t str_to_int(string str)
 {
     int num=0;
     for(int i = 0; i < str.length(); ++i)
@@ -34,18 +34,21 @@ size_t str_to_int(string str)
 /// @brief apply's the button(s) provided to the provided light
 /// @param lights 
 /// @param pressed_buttons 
-void push_button(Inidicator& lights, vector<size_t> pressed_buttons) {
+uint16_t push_button(const uint16_t & lights, const vector<uint16_t>& pressed_button, uint16_t light_len) {
+    uint16_t  ans = lights;
     // XOR op with the specific bit shifted
-    for(const auto& button: pressed_buttons ) {
-        lights.light_status ^= (0x1 << lights.num_lights - 1 - button);
+    for (const auto& each: pressed_button) {
+        ans ^= (0x1 << light_len - 1 - each);
     }
+
+    return ans;
 }
 
 /// @brief takes in a comma seperated string of numbers and returns a vec of ints
 /// @param group_str 
 /// @return 
-vector<size_t> parse_group(string group_str) {
-    vector<size_t> nums{};
+vector<uint16_t> parse_group(string group_str) {
+    vector<uint16_t> nums{};
     stringstream ss(group_str);
     string seg;
 
@@ -65,17 +68,17 @@ vector<LightDiagram> parse_input(vector<string> lines)
     for (const auto& line: lines) {
         stringstream ss_in(line);
         string group_token{};
-        size_t jolts_start = line.find('{');
+        uint16_t jolts_start = line.find('{');
         light_diagrams.push_back(LightDiagram{});
 
         while (ss_in >> group_token) {
 
-            size_t start = group_token.find('(');
-            size_t end = group_token.find(')');
+            uint16_t start = group_token.find('(');
+            uint16_t end = group_token.find(')');
 
             if (group_token.find(']') != string::npos) {
                 // parse lights
-                size_t sq_end = group_token.find(']');
+                uint16_t sq_end = group_token.find(']');
 
                 auto group = group_token.substr(1, sq_end - 1);
                 for (int i = 0; i < group.size(); ++i) {
@@ -96,7 +99,7 @@ vector<LightDiagram> parse_input(vector<string> lines)
                 auto group = parse_group(content);
                 light_diagrams[idx].buttons.push_back(group);
             } else {
-                const auto group = vector<size_t>{str_to_int(content)};
+                const auto group = vector<uint16_t>{str_to_int(content)};
                 light_diagrams[idx].buttons.push_back(group);
             }
 
@@ -107,7 +110,6 @@ vector<LightDiagram> parse_input(vector<string> lines)
     }
     return light_diagrams;
 }
-
 
 int main(int argc, char** argv) {
     std::string path = (argc > 1) ? argv[1] : "test.txt";
@@ -125,23 +127,43 @@ int main(int argc, char** argv) {
     }
 
     auto lds = parse_input(lines);
-    vector<size_t> min_presses{};
+    vector<uint16_t> min_presses{};
+
+    unordered_set<uint16_t > last_set{};
     
     for (auto& ld: lds) {
         
         // stop while loop when the goal matches the current light's status
         bool goal_match = false;
-        size_t presses = 0;
-        unordered_set<uint16_t> states{};
+        uint16_t presses = 1;
+        unordered_set<uint16_t > curr_set{};
+        uint16_t  num;
+
+        // initialize last set
+        for (auto& l: ld.buttons) {
+            last_set.insert(push_button(0, l, 5));
+        }
 
         while(!goal_match) {
             ++presses;
 
-            // keep traversing all paths (with BFS) until we find a match
+            for (auto& state: last_set) {
+                curr_set.emplace(state);
+            }
 
+            // keep traversing all paths (with BFS) until we find a match
+            for (auto& curr: last_set) {
+                for (auto& button: ld.buttons) {
+                    uint16_t  num = push_button(curr, button, ld.indicator.num_lights);
+                    curr_set.emplace(num);
+                }
+            }
             
             // check if match found
-            goal_match = states.find(ld.indicator.light_goal) != states.end();
+            goal_match = curr_set.find(ld.indicator.light_goal) != curr_set.end();
+
+            last_set = curr_set;
+            curr_set.clear();
         }
 
         min_presses.push_back(presses);
